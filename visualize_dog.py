90d9c19ea61cea03705928cd0c887a59afec51e4
AkilanKD <akikd410@gmail.com>
2025-09-28 15:44:45 -0400

    Added shoulder joints, adjusted spacing

--- visualize_dog.py
+++ visualize_dog.py
@@ -20,7 +20,7 @@ def view_floor():
     while True:
         mj.mj_step(model, data)
         print(data.qpos)
-        data.ctrl = 10 * np.sin(t + np.array([0.1, 0.2, 0.3] * 4))
+        data.ctrl = 10 * np.sin(t + np.array([0.0, 0.1, 0.2] * 4))
         t += dt
         time.sleep(dt)
         viewer2.sync()
